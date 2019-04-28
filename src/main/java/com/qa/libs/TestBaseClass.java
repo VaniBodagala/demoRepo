5b0ffbddf4b3f7b02958e276f4da24ac0d222410
vani1987 <aletivani@gmail.com>
2019-04-28 17:09:14 -0500

    all files commited

---
+++ src/main/java/com/qa/libs/TestBaseClass.java
@@ -0,0 +1,8 @@
+package com.qa.libs;
+
+public class TestBaseClass {
+	public void test() {
+		System.out.println("test method");
+	}
+
+}
---
+++ src/main/java/com/qa/util/UtilTestClass.java
@@ -0,0 +1,7 @@
+package com.qa.util;
+
+public class UtilTestClass {
+	public void util() {
+		System.out.println("util method");
+	}
+}
