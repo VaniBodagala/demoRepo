b30a4d00e5459df4dbabe0b5dbec35707215a897
vani1987 <aletivani@gmail.com>
2019-04-28 17:57:18 -0500

    search added and login modified

--- src/main/java/com/qa/pages/LoginPage.java
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -4,4 +4,8 @@ public class LoginPage {
 	public void loginPage() {
 		System.out.println("login page");
 	}
+	public void loginbutton()
+	{
+		System.out.println("another function in login page");
+	}
 }
